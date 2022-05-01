1810690dca1d1f915e3532fdb9c87cdf0d879c20
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-05-01 12:07:48 +0900

    2022/05/01

---
+++ ABC222/A/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	n := nextInt()
+	fmt.Printf("%04d", n)
+}
+
+/*==========================================
+ *             Library
+ *==========================================*/
+
+// 一行ずつ読み込む
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// スペース区切りで読み込む
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
