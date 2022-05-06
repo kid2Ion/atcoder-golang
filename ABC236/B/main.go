d98ec36dc487f6346b677160d52aa2f2dfa19c45
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-05-06 20:08:51 +0900

    2022/05/06

---
+++ ABC236/B/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	n := nextInt()
+	arr := nextIntList(4*n - 1)
+	m := make(map[int]int, 0)
+	for _, v := range arr {
+		m[v]++
+	}
+	for i := 1; i <= n; i++ {
+		if m[i] != 4 {
+			fmt.Println(i)
+		}
+	}
+}
+
+/*==========================================
+ *             Library_origin
+ *==========================================*/
+
+// 一行読み込み
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// intで読み込み(1文字)
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+// floatで読み込み(1文字)
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+// 空白区切り→配列(int)
+func nextIntList(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = nextInt()
+	}
+	return arr
+}
+
+// 空白区切り→配列(float64)
+func nextFloatList(n int) []float64 {
+	arr := make([]float64, n)
+	for i := 0; i < n; i++ {
+		arr[i] = nextFloat64()
+	}
+	return arr
+}
+
+// 空白区切り→配列(string)
+func nextStringList(n int) []string {
+	arr := make([]string, n)
+	for i := 0; i < n; i++ {
+		arr[i] = next()
+	}
+	return arr
+}
+
+// 冪乗(intのままでok)
+func pow(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+// 比較して大きい数字返却
+func max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// 比較して小さい数字返却
+func min(a, b int) int {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+/*==========================================
+ *             Library_default_package
+ *==========================================*/
+
+// 辞書順比較
+// s < t (string同士の比較)
+
+// 文字列→配列
+// arr := strings.Split(s, "")
+
+// 配列→文字列
+// s := strings.Join(arr, "")
+
+// sort(int）(返り値なし)
+// sort.Ints(arr)
+
+// sort(アルファベット順)(返り値なし)
+// sort.Strings(arr)
+
+// 排他的論理和(XOR)(aとbの)
+// a ^ b
