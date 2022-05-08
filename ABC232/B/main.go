ce568f7075075b7e65297cf7891e689577a980ca
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-05-08 17:34:24 +0900

    2022/05/08

---
+++ ABC232/B/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	s, t := next(), next()
+	diff := (int(s[0]) + 26 - int(t[0])) % 26
+	for i := 1; i < len(s); i++ {
+		if (int(s[i])+26-int(t[i]))%26 != diff {
+			fmt.Println("No")
+			return
+		}
+	}
+	fmt.Println("Yes")
+}
+
+/*==========================================
+ *             Library_origin
+ *==========================================*/
+
+// 一行読み込み
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// intで読み込み(1文字)
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+// floatで読み込み(1文字)
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+// 空白区切り→配列(int)
+func nextIntList(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = nextInt()
+	}
+	return arr
+}
+
+// 空白区切り→配列(float64)
+func nextFloatList(n int) []float64 {
+	arr := make([]float64, n)
+	for i := 0; i < n; i++ {
+		arr[i] = nextFloat64()
+	}
+	return arr
+}
+
+// 空白区切り→配列(string)
+func nextStringList(n int) []string {
+	arr := make([]string, n)
+	for i := 0; i < n; i++ {
+		arr[i] = next()
+	}
+	return arr
+}
+
+// 冪乗(intのままでok)(xのy乗)
+func pow(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+// 比較して大きい数字返却
+func max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// 比較して小さい数字返却
+func min(a, b int) int {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// stringのリバースを返す(abcd→dcba)
+func reverse(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
+
+// "Yes","No"の出力
+func PrintBool(a bool) {
+	if a {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+/*==========================================
+ *             Library_default_package
+ *==========================================*/
+
+// 辞書順比較
+// s < t (string同士の比較)
+
+// 文字列→配列
+// arr := strings.Split(s, "")
+
+// 配列→文字列
+// s := strings.Join(arr, "")
+
+// sort(int）(返り値なし)
+// sort.Ints(arr)
+
+// sort(アルファベット順)(返り値なし)
+// sort.Strings(arr)
+
+// 排他的論理和(XOR)(aとbの)
+// a ^ b
+
+// 平方根
+// math.Sqrt(float64(x))
+
+// string(英数字)の切り取り
+// s[1:3]→1文字目から3文字目まで
+// s[:2]→2文字目まで
+
+// string(s)の3文字目
+// s[2]
+
+// s→tは何文字後か(アルファベット)(zの次はa)
+// (s + 26 - t) % 26
